b28370c2ce34e39536f5766255eab6631e0097b7
Johannes H. Gjeraker <johannes.holm.gjeraker@disruptive-technologies.com>
2024-07-23 11:06:35 +0200

    added gallery for irl questing

--- js/gallery.js
+++ js/gallery.js
@@ -1,20 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const galleries = document.querySelectorAll('.gallery');
+    const galleries = document.querySelectorAll('.gallery, .gallery-seichijunrei');
 
     galleries.forEach(gallery => {
         const baseURL = gallery.getAttribute('data-base-url');
         const images = JSON.parse(gallery.getAttribute('data-images'));
 
-        const thumbnailURL = baseURL + "thumbnails/";
-
         images.forEach(image => {
+            const pathSegments = image.split('/');
+            const fileName = pathSegments.pop(); // Get the file name
+            const thumbnailPathSegments = [...pathSegments, 'thumbnails', fileName];
+            const thumbnailURL = baseURL + thumbnailPathSegments.join('/');
+
             const anchor = document.createElement('a');
             anchor.href = baseURL + image;
             anchor.target = "_blank";
             anchor.rel = "noopener noreferrer";
 
             const img = document.createElement('img');
-            img.src = thumbnailURL + image;
+            img.src = thumbnailURL;
             img.loading = "lazy";
 
             anchor.appendChild(img);
